227513e8559f77106a3726bc40b8bdad48629c61
naderhes <naderhesham810@gmail.com>
2026-01-01 03:10:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("JS شغال ✅");
+const correctPassword = "24/4/2002";
+
+function checkPassword() {
+  const input = document.getElementById("password").value.trim();
+  const error = document.getElementById("error");
+
+  if (input === correctPassword) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("content").classList.remove("hidden");
+  } else {
+    error.innerText = "❌ الباسورد غلط، جربي تاني";
+  }
+}
