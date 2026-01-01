bd92f9b949874a3289827cb9699d03f995b7097b
naderhes <naderhesham810@gmail.com>
2026-01-01 03:14:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const correctPassword = "24/4/2002";
 
+// الباسورد
 function checkPassword() {
   const input = document.getElementById("password").value.trim();
   const error = document.getElementById("error");
@@ -7,7 +8,32 @@ function checkPassword() {
   if (input === correctPassword) {
     document.getElementById("login").style.display = "none";
     document.getElementById("content").classList.remove("hidden");
+    document.getElementById("music").play();
   } else {
     error.innerText = "❌ الباسورد غلط، جربي تاني";
   }
+}
+
+// السلايدر
+let current = 0;
+const images = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+  "images/5.jpg"
+];
+
+function showImage() {
+  document.getElementById("sliderImage").src = images[current];
+}
+
+function nextImage() {
+  current = (current + 1) % images.length;
+  showImage();
+}
+
+function prevImage() {
+  current = (current - 1 + images.length) % images.length;
+  showImage();
 }
